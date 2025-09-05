1ffa20f34159c15125d72a90828872a0ea595bf6
atharv7219 <atharvmehta7219@gmail.com>
2025-09-05 21:36:37 +0530

    day 13 02.c

---
+++ day 13 02.c
@@ -0,0 +1,15 @@
+//Q26: Write a program to print numbers from 1 to n.
+
+#include <stdio.h>
+ int main()
+ {
+    int i,n;
+    printf("Enter any number");
+    scanf("%d",&n);
+
+    for(i=1;i<=n;i++)
+    {
+        printf("%d",i );
+        printf("\n");
+    }
+ }
